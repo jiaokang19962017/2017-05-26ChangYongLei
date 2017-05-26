3d3d6547a5a942a88e760d58bb7d6b6d7af0e2b5
jiaokang <Administrator@HP201-1>
2017-05-26 10:43:28 +0800

    字符串方法

--- 常用类/Program.cs
+++ 常用类/Program.cs
@@ -10,6 +10,27 @@ namespace 常用类
     {
         static void Main(string[] args)
         {
+            Random rd = new Random();
+            int[] array = new int[7];
+            int temp = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                temp = rd.Next(1, 7);
+                int j = 0;
+                do
+                {
+                    if (temp == array[i])
+                    {
+                        i--;
+                        break;
+                    }
+                } while ();
+
+            }
+
+
+            
         }
+
     }
 }
